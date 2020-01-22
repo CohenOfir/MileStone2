b14fa5b789c202251f6e91ddd16a043b4e76dd8d
CohenOfir <59443707+CohenOfir@users.noreply.github.com>
2020-01-23 00:45:26 +0200

    update

--- CacheManager.h
+++ CacheManager.h
@@ -6,17 +6,11 @@
 
 using namespace std;
 
-
 class CacheManager {
-protected:
-    map <string,string> data;
-    mutable pthread_mutex_t mutex;
-
 public:
     virtual bool isInCache(string prob) = 0;
     virtual string getSolution(string prob) = 0;
     virtual void setSolution(string prob, string solution) = 0;
-    virtual void saveToDisk(string prob, string solution)= 0;
     virtual ~CacheManager() = default;
 };
 
